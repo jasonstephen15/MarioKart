bd1a61c0e01e8472542fdd180f963df885bec7f0
Jason Stephen <jason@jasons-macbook-pro.local>
2019-03-23 21:19:58 -0500

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Jason Stephen on 3/23/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
